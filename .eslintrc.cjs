de802eeee3cc6ed79dc6dd16c23a791c131d16da
trycatchfinally <finally.catch.try@gmail.com>
2023-01-01 22:58:37 +0200

    Init files

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:sonarjs/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json']
+  },
+  plugins: ['@typescript-eslint', 'import', 'sonarjs', 'prettier'],
+  rules: {
+    'no-empty': ['warn'],
+    'sonarjs/no-ignored-return': ['off'],
+    '@typescript-eslint/no-misused-promises': ['error', {checksVoidReturn: false}],
+    '@typescript-eslint/no-unsafe-assignment': ['off'],
+    '@typescript-eslint/no-unsafe-argument': ['off'],
+    '@typescript-eslint/no-unsafe-member-access': ['off']
+  }
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+iGadgets REST API server. Technology stack: ts, express, mongodb
+
+подключать монго локальный/удаленный сервер
+статику раздавать на нджинкс
